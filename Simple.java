ab1347ac1bc0193129f10e8c3645415c9e6ae4aa
siri9398 <Sreevani.d09@gmail.com>
2025-05-26 22:36:10 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,12 @@
+import java.util.Scanner
+public class Simple calculator{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        add num1=Scanner.nextAdd();
+        System.out.println("Enter the second number:");
+        add num2=Scanner.nextAdd();
+        add sum=num1+num2;
+        System.out.println("The sum is:"+sum);
+    }
+}
